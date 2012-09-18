a6351bbd1fbc578c2dcaa7c22ca85b047b8df463
Colin <colin@woofound.com>
2012-09-18 14:00:07 -0400

    Added skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Badger::Application.config.secret_token = '7370ad7eb0dea7cd468099c267007e6d8d1421e8c4ade2b5a47eba85435645d7a38125544b7f6ff6f0cb3c159946cc87df126dfb01baab5d24c66bd0598160a6'
