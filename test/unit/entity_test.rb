cccfb8e4ad2776465e39a2a9980efee21e17f2d2
Colin <colin@woofound.com>
2012-09-18 16:07:32 -0400

    Added Entity model and validation/fixture tests

---
+++ test/unit/entity_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class EntityTest < ActiveSupport::TestCase
+  # Validations
+  test "should not create a entity without a name" do
+    entity = entities(:empty)
+    assert !entity.save
+  end
+
+  test "should create a valid entity" do
+    entity = entities(:one)
+    assert entity.save
+    assert_equal entity.name, "TestEntity"
+    assert_equal entity.address, "123 Test St."
+  end
+end
