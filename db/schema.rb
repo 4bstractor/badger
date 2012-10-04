da8a783f0c0f3e0e23bd5278544ec47c61a4e159
Colin <colin@woofound.com>
2012-10-04 15:54:23 -0400

    Added extra data to users to hold name and phone number, no entry point yet

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121003033912) do
+ActiveRecord::Schema.define(:version => 20121004193900) do
 
   create_table "entities", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(:version => 20121003033912) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.integer  "entity_id"
+    t.string   "name"
+    t.string   "phone"
   end
 
 end
