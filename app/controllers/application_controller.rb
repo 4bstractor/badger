0acf86992d9315eaa9226595ed67358e005fb19e
Colin <theunderstander@gmail.com>
2013-09-17 01:46:36 -0400

    Weird find not working when user table is empty, fixed with helper find for now, change to where later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,12 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate!
+    puts current_user
     redirect_to(sign_in_url) unless current_user
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
