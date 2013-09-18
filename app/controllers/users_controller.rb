34ec6ecc96534d9e760ebc7594b878148f26210e
Colin <theunderstander@gmail.com>
2013-09-18 02:48:11 -0400

    Made registration sign in the user for now, may disable later

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
 
     if @user.save
+      # May remove if registration is added
+      session[:user_id] = @user.id
       redirect_to root_url, :notice => "Signed Up!"
     else
       render "new"
